951d53940aebe3b57e9519bf0154af5eddc633d4
Adam Harvey <adamharvey99@live.co.uk>
2023-06-23 17:48:29 +0100

    inital setup

---
+++ src/main/java/com/example/nccBackend/services/googleCalendarService.java
@@ -0,0 +1,21 @@
+package com.example.nccBackend.services;
+
+import com.example.nccBackend.models.calendarEvent;
+import com.example.nccBackend.models.verseOftheDay;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class googleCalendarService {
+
+    public calendarEvent getVerseOfTheDay() throws IOException {
+        URL url = new URL("https://www.googleapis.com/calendar/v3/calendars/euivajuod3cpk9vcigr9juvo8s@group.calendar.google.com/events?key=AIzaSyDHlhT30ksmGeflzVxBgLzNdkhHIZrs55o");
+        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+        con.setRequestMethod("GET");
+
+
+    }
+}
