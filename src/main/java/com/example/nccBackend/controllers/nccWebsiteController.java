cdaeed2dab35d721226853fe72c80f2992f5929a
Adam Harvey <adamharvey99@live.co.uk>
2023-06-24 23:36:06 +0100

    google calendar api backend

--- src/main/java/com/example/nccBackend/controllers/nccWebsiteController.java
+++ src/main/java/com/example/nccBackend/controllers/nccWebsiteController.java
@@ -1,24 +1,37 @@
 package com.example.nccBackend.controllers;
 
+import com.example.nccBackend.services.googleCalendarService;
 import com.example.nccBackend.services.verseOfTheDayService;
-import com.example.nccBackend.models.verseOftheDay;
+import com.example.nccBackend.models.calendarEvent;
+import net.minidev.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.concurrent.ExecutionException;
 
 @RestController
+@CrossOrigin
 public class nccWebsiteController {
 
     @Autowired
     verseOfTheDayService VerseOftheDayService;
 
+    @Autowired
+    googleCalendarService GoogleCalendarService;
+
 
     @GetMapping("/verse")
     public StringBuffer getVerse() throws InterruptedException, ExecutionException, IOException {
         return VerseOftheDayService.getVerseOfTheDay();
+    }
+
+    @GetMapping("/calendarEvents")
+    public StringBuffer getCalendar() throws InterruptedException, ExecutionException, IOException{
+        return GoogleCalendarService.getCalendarEvents();
     }
 }
--- src/main/java/com/example/nccBackend/services/googleCalendarService.java
+++ src/main/java/com/example/nccBackend/services/googleCalendarService.java
@@ -2,20 +2,52 @@ package com.example.nccBackend.services;
 
 import com.example.nccBackend.models.calendarEvent;
 import com.example.nccBackend.models.verseOftheDay;
+import com.google.gson.Gson;
+import net.minidev.json.JSONObject;
+import org.apache.tomcat.util.json.JSONParser;
+import org.springframework.stereotype.Service;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.Arrays;
+import java.util.List;
 
+
+@Service
 public class googleCalendarService {
 
-    public calendarEvent getVerseOfTheDay() throws IOException {
+    public StringBuffer getCalendarEvents() throws IOException {
         URL url = new URL("https://www.googleapis.com/calendar/v3/calendars/euivajuod3cpk9vcigr9juvo8s@group.calendar.google.com/events?key=");
         HttpURLConnection con = (HttpURLConnection) url.openConnection();
         con.setRequestMethod("GET");
-
-
+//        con.setRequestProperty("Content-Type", "application/json");
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(con.getInputStream()));
+        String inputLine;
+        StringBuffer content = new StringBuffer();
+        while ((inputLine = in.readLine()) != null) {
+            content.append(inputLine);
+        }
+        in.close();
+        return content;
     }
+
+//    public static JSONObject getCalendarEvents() throws IOException {
+//        URL url = new URL("https://www.googleapis.com/calendar/v3/calendars/euivajuod3cpk9vcigr9juvo8s@group.calendar.google.com/events?key=AIzaSyDHlhT30ksmGeflzVxBgLzNdkhHIZrs55o");
+//        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+//        con.setRequestMethod("GET");
+//        con.setRequestProperty("Content-Type", "application/json");
+//        BufferedReader in = new BufferedReader(
+//                new InputStreamReader(con.getInputStream()));
+//        String inputLine;
+//        StringBuffer content = new StringBuffer();
+//        while ((inputLine = in.readLine()) != null) {
+//            content.append(inputLine);
+//        }
+//        in.close();
+//        return null;
+//    }
 }
